304c6dda1f31d7e53f7d178c7e58a89854bd804d
Luonghiii <luonghii123@gmail.com>
2025-12-02 17:05:30 +0700

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import Header from "@/components/Header";
+import Banner from "@/components/Banner";
+import MovieRow from "@/components/MovieRow";
+import { movies } from "@/data/movies";
+
+export default function Home() {
+  return (
+    <main className="bg-black min-h-screen text-white">
+      <Header />
+      <Banner />
+      <div className="mt-[-4rem] space-y-10 pb-10">
+        <MovieRow title="Trending Now" items={movies} />
+        <MovieRow title="Top Picks for You" items={movies} />
+        <MovieRow title="New Releases" items={movies} />
+      </div>
+    </main>
+  );
+}
