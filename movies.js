223055d97fb357b41867e289197cd3bf723a1383
Luonghiii <luonghii123@gmail.com>
2025-12-02 17:03:42 +0700

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,50 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Extraction 2",
+    thumbnail:
+      "https://image.tmdb.org/t/p/w342/7gKI9hpEMcZUQpNgKrkDzJpbnNS.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/ArDXtqhG3yJK45Tky8uBoijCKq.jpg",
+  },
+  {
+    id: 2,
+    title: "The Witcher",
+    thumbnail:
+      "https://image.tmdb.org/t/p/w342/zQpQe1jLK0xH8ApYkvKPtZL0Dta.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/7vjaCdMw15FEbXyLQTVa04URsPm.jpg",
+  },
+  {
+    id: 3,
+    title: "Alice in Borderland",
+    thumbnail:
+      "https://image.tmdb.org/t/p/w342/iPI3cHGN3gt36HrZ3eKohIP6Xcj.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/6hfjGfrKcQfQMfCqCw48p2UekLJ.jpg",
+  },
+  {
+    id: 4,
+    title: "Money Heist",
+    thumbnail:
+      "https://image.tmdb.org/t/p/w342/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/c8V3cVJqEo9C5pucs5cjox96D65.jpg",
+  },
+  {
+    id: 5,
+    title: "Stranger Things",
+    thumbnail:
+      "https://image.tmdb.org/t/p/w342/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/49WJfeN0moxb9IPfGn8AIqMGskD.jpg",
+  },
+  {
+    id: 6,
+    title: "Dark",
+    thumbnail:
+      "https://image.tmdb.org/t/p/w342/apbrv2nYfpk1zEYcBfxX7njNUmZ.jpg",
+    backdrop:
+      "https://image.tmdb.org/t/p/original/nlTUgb9vYtH7kWFaVFIxZlAQ9wP.jpg",
+  },
+];
